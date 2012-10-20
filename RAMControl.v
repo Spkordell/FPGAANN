3b22c9d8aedf6129a1e5a1254900ee4d1bee2b74
Spkordell <mask@mask>
2012-10-20 18:27:11 -0400

    Finished Asynchronous Ram Controller

--- RAMControl.v
+++ RAMControl.v
@@ -18,17 +18,18 @@
 // Additional Comments: 
 //This module is meant to communicate with the onboard RAM chip in asynchronous Mode
 //////////////////////////////////////////////////////////////////////////////////
-module RAMControl(testData,LED,MemAdr,MemDB,RamCE,MemOE,MemWE,RamAdv,RamClk,RamLB,RamUB,FlashCE);
+module RAMControl(clk,Instruction,latch,ramBusDataOut,ramBusDataIn,ramBusAddr,MemAdr,MemDB,RamCE,MemOE,MemWE,RamAdv,RamClk,RamLB,RamUB,FlashCE,Ready);
 
-input testData[15:0];
+input clk;
+input Instruction;
+input latch;
+input [15:0]ramBusDataIn;
+output reg [15:0]ramBusDataOut;
+input [23:1]ramBusAddr;
 
-output LED;
-
-assign LED=1;
-
-inout  [15:0] MemDB;
-output [23:1]MemAdr;
-output regRamCE;
+inout  [15:0]MemDB;
+output reg [23:1]MemAdr;
+output reg RamCE;
 output reg MemOE;
 output reg MemWE;
 output RamAdv;
@@ -36,6 +37,8 @@ output RamClk;
 output RamLB;
 output RamUB;
 output FlashCE;
+
+output reg Ready;
 
 //make inout port available
 wire [15:0] MemDBIn;
@@ -51,5 +54,65 @@ assign RamLB=0;
 assign RamUB=0;
 assign FlashCE=1;
 
+parameter READ=0;
+parameter WRITE=1;
+parameter RESET=2;
+parameter READWAIT0=3;
+parameter READWAIT1=4;
+parameter READWAIT2=5;
+parameter READWAIT3=6;
+parameter WRITEWAIT0=7;
+parameter WRITEWAIT1=8;
+parameter WRITEWAIT2=9;
+parameter WRITEWAIT3=10;
+reg [3:0]ramState=RESET;
 
+always @(posedge clk) begin
+	case (ramState)
+		RESET: begin
+			RamCE<=1;
+			MemOE<=1;
+			MemWE<=1;
+			MemDBOE<=0;
+			MemAdr<=ramBusAddr;
+			if (latch) begin
+				Ready<=0;
+				if (Instruction==READ) begin
+					ramState<=READ;
+				end else begin
+					ramState<=WRITE;		
+					MemDBOut<=ramBusDataIn;
+				end
+			end
+		end 	
+		READ: begin
+			MemDBOE<=0;
+			RamCE<=0;
+			MemOE<=0;
+			MemWE<=1;
+			ramState<=READWAIT0;
+		end
+		WRITE: begin
+			MemDBOE<=1;
+			RamCE<=0;
+			MemWE<=0;
+			ramState<=WRITEWAIT0;
+		end
+		READWAIT0: ramState<=READWAIT1;
+		READWAIT1: ramState<=READWAIT2;
+		READWAIT2: ramState<=READWAIT3;
+		READWAIT3: begin
+			ramState<=RESET;
+			Ready<=1;
+			ramBusDataOut<=MemDB;			
+		end
+		WRITEWAIT0: ramState<=WRITEWAIT1;
+		WRITEWAIT1: ramState<=WRITEWAIT2;
+		WRITEWAIT2: ramState<=WRITEWAIT3;	
+		WRITEWAIT3: begin
+			ramState<=RESET;
+			Ready<=1;
+		end		
+	endcase
+end
 endmodule
--- RamTest.v
+++ RamTest.v
@@ -18,10 +18,43 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module RamTest();
+module RamTest(clk,MemDB,MemAdr,RamCE,MemOE,MemWE,RamAdv,RamClk,RamLB,RamUB,FlashCE);
 
-reg testData;
+parameter READ=0;
+parameter WRITE=1;
 
-RAMControl RC(testData);
+input clk;
+inout  [15:0]MemDB;
+output [23:1]MemAdr;
+output RamCE;
+output MemOE;
+output MemWE;
+output RamAdv;
+output RamClk;
+output RamLB;
+output RamUB;
+output FlashCE;
+
+reg ramInstruction;
+reg ramLatch;
+wire [15:0]ramBusDataOut;
+reg [15:0]ramBusDataIn=0;
+reg [23:1]ramBusAddr=0;
+wire ramReady;
+
+RAMControl RC(clk,ramInstruction,ramLatch,ramBusDataOut,ramBusDataIn,ramBusAddr,MemAdr,MemDB,RamCE,MemOE,MemWE,RamAdv,RamClk,RamLB,RamUB,FlashCE,ramReady);
+
+always @(posedge clk) begin
+	if (ramReady) begin
+		ramInstruction<=WRITE;
+		ramBusDataIn<=ramBusDataIn+1;
+		ramBusAddr<=ramBusAddr+1;
+		ramLatch<=1;
+	end
+end
+
+always @(negedge clk) begin
+	ramLatch<=0;
+end
 
 endmodule
